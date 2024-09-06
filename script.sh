5b2efa386f50268a8847ddee474bd52d6b79241d
Marco <maguilar@patriotsoftware.com>
2024-09-06 19:00:28 -0400

    updated notes

--- script.sh
+++ script.sh
@@ -23,13 +23,13 @@ get_database_connection_settings $SECRET_ID
 
 if [ $ACTION = "Delete" ]; then
 
-    echo "Deleting $DATABASE database.."
+    echo "Deleting '$DATABASE' database.."
 
     psql -U $DBUSERNAME -h $DBHOST -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '${DATABASE}';"
     psql -U $DBUSERNAME -h $DBHOST -d postgres -c "DROP DATABASE \"${DATABASE}\";"
     psql -U $DBUSERNAME -h $DBHOST -d control_center -c "UPDATE log.branch_database SET deleted_on = NOW() WHERE database_name = '${DATABASE}' AND deleted_on IS NULL;"
 
-    echo "Database delete complete."
+    echo "'$DATABASE' database has been deleted."
 
 elif [[ $ACTION = "Create" ]] || [[ $ACTION = "Recreate" ]]; then
     
